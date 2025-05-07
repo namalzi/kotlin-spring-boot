7796f6e480a0fe47ebdbec2a60383d7eca07ad29
namalzi <notlaterbutnow723@gmail.com>
2025-05-08 00:29:05 +0900

    테스트 코드 추가

--- src/test/kotlin/org/example/kopring/KopringApplicationTests.kt
+++ src/test/kotlin/org/example/kopring/KopringApplicationTests.kt
@@ -1,13 +1,33 @@
 package org.example.kopring
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class KopringApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class KopringApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @BeforeAll
+    fun setup(){
+        println(">> Setup")
+    }
+
+    @AfterAll
+    fun teardown(){
+        println(">> Tear down")
+    }
 
     @Test
-    fun contextLoads() {
+    fun `Assert blog page title, content and status code`() {
+        val entity = restTemplate.getForEntity<String>("/")
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(entity.body).contains("<h1>Blog</h1>")
     }
 
 }
